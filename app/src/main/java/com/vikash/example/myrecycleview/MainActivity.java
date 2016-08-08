2550e0e2cd1506b6c1eebe43cc17ad70771b203f
vkdinventor@gmail.com <vkdinventor@gmail.com>
2016-08-08 12:52:34 +0530

    Method to scan wifi and display ssid is added to MainActivity.java

--- app/src/main/java/com/vikash/example/myrecycleview/MainActivity.java
+++ app/src/main/java/com/vikash/example/myrecycleview/MainActivity.java
@@ -1,23 +1,61 @@
 package com.vikash.example.myrecycleview;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView listView;
+    WifiManager wifi;
+    String wifis[];
+    WifiScanReceiver wifiReciever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView hlist = (RecyclerView)findViewById(R.id.hlistview);
+        listView= (RecyclerView)findViewById(R.id.hlistview);
 
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        hlist.setLayoutManager(layoutManager);
-        MyAdapter listAdapter= new MyAdapter();
-        hlist.setAdapter(listAdapter);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        listView.setLayoutManager(layoutManager);
 
+        wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+        wifiReciever = new WifiScanReceiver();
+        wifi.startScan();
 
+    }
+
+    protected void onPause() {
+        unregisterReceiver(wifiReciever);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(wifiReciever, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    private class WifiScanReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+            List<ScanResult> wifiScanList = wifi.getScanResults();
+            wifis = new String[wifiScanList.size()];
+
+            for(int i = 0; i < wifiScanList.size(); i++){
+                wifis[i] = ((wifiScanList.get(i)).toString());
+            }
+            listView.setAdapter(new MyAdapter(MainActivity.this,wifis));
+
+        }
     }
 }
--- app/src/main/java/com/vikash/example/myrecycleview/MyAdapter.java
+++ app/src/main/java/com/vikash/example/myrecycleview/MyAdapter.java
@@ -1,10 +1,14 @@
 package com.vikash.example.myrecycleview;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by einfochips on 28/7/16.
@@ -21,6 +25,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         }
     }
 
+    Context mContext;
+    String[] mSSID;
+    MyAdapter(Context context,String[] ssids){
+        mContext=context;
+        mSSID=ssids;
+    }
+
     @Override
     public int getItemCount() {
         return 50;
@@ -29,12 +40,44 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem,parent,false);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                // Get the layout inflater
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                builder.setView(inflater.inflate(R.layout.dialog_layout, null));
+                builder.setTitle("Connect to WiFi");
+                        // Add action buttons
+                builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // sign in the user ...
+                                Toast.makeText(mContext, "Connecting..", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                builder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(mContext, "Cancled", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+            }
+
+        }
+    );
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
-        holder.textviewheader.setText("Medium: "+position);
-        holder.textViewfooter.setText("Small: "+position);
+        holder.textviewheader.setText(mSSID[position]);
     }
 }
